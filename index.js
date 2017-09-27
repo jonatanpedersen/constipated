ebf5de622edd4610f38f0c4155b478bf25963558
Jonatan Pedersen <jp@jonatanpedersen.com>
2017-09-27 22:30:40 +0200

    adds tests, fixes bugs

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var glob = require('glob');
+var fs = require('fs');
 
 module.exports = {relieveGlob, relieveFile, relieveString};
 
 function relieveGlob (pattern) {
-	var files = flob.sync(pattern);
+	var files = glob.sync(pattern);
 
 	files.forEach(relieveFile);
 };
--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 var {relieveGlob, relieveFile, relieveString} = require('../');
 var {expect} = require('chai');
+var {copySync, emptyDirSync} = require('fs-extra');
+var {compareSync} = require('dir-compare');
 
 describe('constipated', () => {
 	describe('relieveString', () => {
@@ -8,6 +10,63 @@ describe('constipated', () => {
 			var expected = 'var constipated = false';
 
 			expect(actual).to.eql(expected);
+		});
+	});
+
+	describe('relieveFile', () => {
+		var inputDir, expectedDir, actualDir, actualFile;
+
+		before(() => {
+			inputDir = './test/fixtures/file/input';
+			expectedDir = './test/fixtures/file/expected';
+			actualDir = './test/fixtures/file/actual';
+			actualFile = './test/fixtures/file/actual/foobar.js';
+			emptyDirSync(actualDir);
+			copySync(inputDir, actualDir);
+			relieveFile(actualFile);
+
+		});
+
+		it('should relieve the file', () => {
+			comparison = compareSync(actualDir, expectedDir, {
+				compareContent: true
+			});
+
+			if (!comparison.same) {
+				var message = JSON.stringify(comparison.diffSet.filter(diff => diff.state !== 'equal'), null, 4);
+				var err = new Error(message);
+				err.comparison = comparison;
+
+				throw err;
+			}
+		});
+	});
+
+	describe('relieveGlob', () => {
+		var inputDir, expectedDir, actualDir, glob;
+
+		before(() => {
+			inputDir = './test/fixtures/glob/input';
+			expectedDir = './test/fixtures/glob/expected';
+			actualDir = './test/fixtures/glob/actual';
+			glob = './test/fixtures/glob/actual/*.js';
+			emptyDirSync(actualDir);
+			copySync(inputDir, actualDir);
+			relieveGlob(glob);
+		});
+
+		it('should relieve the glob', () => {
+			comparison = compareSync(actualDir, expectedDir, {
+				compareContent: true
+			});
+
+			if (!comparison.same) {
+				var message = JSON.stringify(comparison.diffSet.filter(diff => diff.state !== 'equal'), null, 4);
+				var err = new Error(message);
+				err.comparison = comparison;
+
+				throw err;
+			}
 		});
 	});
 });
