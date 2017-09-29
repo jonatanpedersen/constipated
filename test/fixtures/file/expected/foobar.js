101918b6041c973d8afa895cd09ddb3ad9742f72
Jonatan Pedersen <jp@jonatanpedersen.com>
2017-09-29 19:59:20 +0200

    adds tests

--- test/fixtures/file/expected/foobar.js
+++ test/fixtures/file/expected/foobar.js
@@ -4,3 +4,6 @@ var bar = false;
 (function () {
 	var baz = 1, qux = 2;
 })();
+
+var someconst = 5;
+let notconst = 3;
--- test/fixtures/file/input/foobar.js
+++ test/fixtures/file/input/foobar.js
@@ -4,3 +4,6 @@ const bar = false;
 (function () {
 	const baz = 1, qux = 2;
 })();
+
+const someconst = 5;
+let notconst = 3;
